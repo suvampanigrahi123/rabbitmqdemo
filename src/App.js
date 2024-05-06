0bdbab31f923b3c18adcc7abc10a6e88d4f87a34
Suvam Panigrahi <suvampanigrahi143@gmail.com>
2024-05-06 11:16:00 +0530

    intial mq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Stomp from 'stompjs';
+import { useEffect, useState } from 'react';
 function App() {
+  const [stompClient, setStompClient] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Connect to WebSocket endpoint
+    const socket = new WebSocket('ws://127.0.0.1:15674/ws');
+    const stomp = Stomp.over(socket);
+
+    // Connect to STOMP
+    stomp.connect({}, () => {
+      console.log('Connected to WebSocket');
+      setStompClient(stomp);
+      
+      // Subscribe to topic
+      stomp.subscribe('/topic/messages', (message) => {
+        const newMessage = JSON.parse(message.body);
+        setMessages((prevMessages) => [...prevMessages, newMessage?.message]);
+      });
+    });
+
+    return () => {
+      if (stompClient) {
+        stompClient.disconnect();
+      }
+    };
+  }, []);
+  const sendMessage = () => {
+    if (stompClient) {
+      stompClient.send('/queue/my_queue', {}, JSON.stringify({ message: 'Hello RabbitMQ!' }));
+    }
+  };
+  console.log('MEsagggggggggggggg',messages);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <h1>Messages:</h1>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
     </div>
   );
 }
